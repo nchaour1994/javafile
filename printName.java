fc91358cb60ade8126fe8c55142056d8f6215351
nabil <nchaour6@gmail.com>
2021-12-21 16:59:42 -0500

    added method print

---
+++ printName.java
@@ -0,0 +1,14 @@
+package datatype;
+
+public class printName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String firstName = "nabil";
+       String lastName = "chaour";
+       String id = "30395ny";
+       
+       System.out.println("my name is "+firstName+" "+lastName+" and this is my id number "+ id );
+	}
+
+}
